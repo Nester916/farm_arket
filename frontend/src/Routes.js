38f893881771626c835608501af46e15afd477da
Naimur Neer <neercool169@gmail.com>
2024-07-04 03:47:12 +0600

    frontend_login

---
+++ frontend/src/Routes.js
@@ -0,0 +1,5 @@
+import Login from "../src/pages/loginpg.jsx";
+
+export{
+    Login
+}
---
+++ frontend/src/pages/loginpg.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Login from "../components/loginpg/loginpg.jsx";
+
+const login = () => {
+    return(
+        <div><Login /></div>
+    )
+}
+export default Login
