d455d1c62364a9ee10bc4ba14007740a0b4935af
GeektheBigParadox <geek.paradox.00@gmail.com>
2024-07-10 16:49:35 +0600

    necessary changes

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -4,13 +4,23 @@ const User = require("../model/user");
 const router = express.Router();
 const {upload} = require("../multer");
 const ErrorHandler = require("../utils/errorhandler");
+const fs = require("fs")
 
 router.post("/create-user", upload.single("file"), async (req, res, next) => {
+  try{
   const { username, fullname, email, password } = req.body;
   const userEmail = await User.findOne({ email });
 
   if (userEmail) {
-    return next(new ErrorHandler("User already exists", 400));
+      const filename = req.file.filename
+      const filePath = `uploads/${filename}`;
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json()
+        }
+      })
+
   }
 
   const filename = req.file.filename;
@@ -23,7 +33,11 @@ router.post("/create-user", upload.single("file"), async (req, res, next) => {
     email: email,
     password: password,
   };
-  console.log(user);
+  const newUser = await User.create(user);
+  res.status(201).json({
+    success: true,
+    newUser,
+  });
 });
 
 module.exports = router;
