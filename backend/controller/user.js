a32dca114a035102ff6285c00277b1930204137c
Naimur Neer <neercool169@gmail.com>
2024-07-10 22:46:42 +0600

    spacing

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -2,45 +2,45 @@ const express = require("express");
 const path = require("path");
 const User = require("../model/user");
 const router = express.Router();
-const {upload} = require("../multer");
+const { upload } = require("../multer");
 const ErrorHandler = require("../utils/errorhandler");
-const fs = require("fs")
+const fs = require("fs");
 
 router.post("/create-user", upload.single("file"), async (req, res, next) => {
-  try{
-  const { username, fullname, email, password } = req.body;
-  const userEmail = await User.findOne({ email });
+  try {
+    const { username, fullname, email, password } = req.body;
+    const userEmail = await User.findOne({ email });
 
-  if (userEmail) {
-      const filename = req.file.filename
+    if (userEmail) {
+      const filename = req.file.filename;
       const filePath = `uploads/${filename}`;
       fs.unlink(filePath, (err) => {
         if (err) {
           console.log(err);
-          res.status(500).json({ message: "Error deleting file"});        
+          res.status(500).json({ message: "Error deleting file" });
         } else {
-          res.json({message: "File deleted successfully"});
+          res.json({ message: "File deleted successfully" });
         }
       });
       return next(new ErrorHandler("User already exists", 400));
-  }
+    }
 
-  const filename = req.file.filename;
-  const fileUrl = path.join(filename);
+    const filename = req.file.filename;
+    const fileUrl = path.join(filename);
 
-  const user = {
-    username: username,
-    fullname: fullname,
-    email: email,
-    password: password,
-    avatar: fileUrl,
-  };
-  const newUser = await User.create(user);
-  res.status(201).json({
-    success: true,
-    newUser,
-  });
-  } catch(error){
+    const user = {
+      username: username,
+      fullname: fullname,
+      email: email,
+      password: password,
+      avatar: fileUrl,
+    };
+    const newUser = await User.create(user);
+    res.status(201).json({
+      success: true,
+      newUser,
+    });
+  } catch (error) {
     return next(new ErrorHandler(error.message), 400);
   }
 });
