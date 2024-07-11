b1563ad15c07c0a045cdb697b44108f0bcf9fcf7
7avir <163576853+7avir@users.noreply.github.com>
2024-07-11 06:33:28 +0600

    cgprofile and shoppingcart frontend

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -2,11 +2,12 @@ import react, { useState } from 'react';
 import { Link } from "react-router-dom";
 import styles from "../../styles/styles";
 import { productData} from "../../staticdata";
-import {AioutlineSearch} from "react-icons/ai";
+import {AioutlineSearch,AiOutlineHeart,AioutlineShoppingCart} from "react-icons/ai";
 import {ToIosArrowforward} from "react-icons?io";
 import {BiMenuAltLeft} from "react-icons/bi";
 import DropDown from "./DropDown";
 import Navbar from "./Navbar";
+import {cgprofile} from "react-icons/cg"
 
 const Header = ({activeHeading}) => {
     const [searchTerm,setSearchTerm] = useState("");
@@ -105,7 +106,7 @@ const Header = ({activeHeading}) => {
                 <div classname ={"${styles.normalFlex}"}>
                     <Navbar active ={activeHeading}/>    
                 </div>    
-                   <div>
+                   <div className='flex'>
                       <div className={'${styles.normalFlex}'}>
                         <div className='relative cursor-pointer mr-[15px]'>
                             <AioutlineHeart
@@ -115,8 +116,27 @@ const Header = ({activeHeading}) => {
                             <span className='absolute right-0 top-0 rounded-full bg-[#3bc177] w-4 h-4 top right p-0 m-0 text-white font-mono text-[12px] leading-tight text-center '>
 
                             </span>
-                    </div>          
-
+                      </div> 
+                      <div className={'${styles.normalFlex}'}>
+                        <div className='relative cursor-pointer mr-[15px]'>
+                            <AioutlineShoppingCart
+                            size = {30}
+                            color = "rgb(255 255 255/83%)"
+                            />
+                            <span className='absolute right-0 top-0 rounded-full bg-[#3bc177] w-4 h-4 top right p-0 m-0 text-white font-mono text-[12px] leading-tight text-center '>
+                            1
+                            </span>
+                       </div> 
+                       <div className={'${styles.normalFlex}'}>
+                        <div className='relative cursor-pointer mr-[15px]'>
+                            <link to ="/login">
+                            <CgProfile
+                            size = {30}
+                            color = "rgb(255 255 255/83%)"
+                            />
+                            </link>
+                      </div>      
+                    </div>           
             </div>
 
         </div>
