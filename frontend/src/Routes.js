e36a493fa321a556cf4147c9d537aedc95e97518
7avir <163576853+7avir@users.noreply.github.com>
2024-09-03 05:00:58 +0600

    Hero Frontend

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,7 +1,7 @@
 import LoginPage from "../pages/Login";
 import SignupPage from "../pages/SignupPage";
 import ActivationPage from "../pages/ActivationPage";
-import HomePage from "../src/pages/HomePage";
+import HomePage from "../src/pages/HomePage.jsx";
 export{
     LoginPage,
     SignupPage,
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import react from 'react'
 import Header from "../components/Layout/Header";
+import hero from "../components/Route/Hero";
 
 const Homepage = () => {
     return (
       <div>
         <Header activeHeading = {1}/>
+        <hero />
       </div>
     )
   }
