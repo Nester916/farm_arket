2e51ff79d5f77cb3517114be9df0b5e7b2c9e266
Naimur Neer <neercool169@gmail.com>
2024-09-02 07:43:00 +0600

    auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login, Signup, Activation } from "./Routes.js";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import axios from 'axios';
-import {server} from "./server";
+import Market from "./redux/market.js";
+import { loadUser } from "./redux/actions/user";
 
 
 const App = () => {
   useEffect(() => {
-    axios.get(`${server}/user/getuser`,{withCredentials:true}).then((res) => {
-      toast.success(res.data.message);
-    }).catch((err) => {
-      toast.error(err.response.data.message);
-    });
+    Market.dispatch(loadUser());
   }, []);
   return (
     <BrowserRouter>
