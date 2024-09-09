9ac337f6188eec07ec5df7b06b780742eb0c41b5
Naimur Neer <geek.paradox.00@gmail.com>
2024-09-09 22:47:55 +0600

    new

---
+++ frontend/src/routes/Routes.js
@@ -0,0 +1,10 @@
+import Login1 from "../pages/loginpg";
+import Signup1 from "../pages/signuppg";
+import Activation1 from "../pages/activationpg";
+import Home1 from "../pages/homepg";
+export{
+    Login1,
+    Signup1,
+    Activation1,
+    Home1,
+};
