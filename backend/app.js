86124cb3e3d73a6eb631f3635d664bd21f5c0843
7avir <163576853+7avir@users.noreply.github.com>
2024-09-10 20:03:53 +0600

    fixes

--- backend/app.js
+++ backend/app.js
@@ -38,7 +38,7 @@ const shop = require("./controller/shop");
 const product = require("./controller/product");
 const event = require("./controller/event");
 const coupon = require("./controller/couponCode");
-const payment = require("./controller/payment");
+//const payment = require("./controller/payment");
 const order = require("./controller/order");
 const withdraw = require("./controller/withdraw");
 
@@ -48,7 +48,7 @@ app.use("/api/v2/shop", shop);
 app.use("/api/v2/product", product);
 app.use("/api/v2/event", event);
 app.use("/api/v2/coupon", coupon);
-app.use("/api/v2/payment", payment);
+//app.use("/api/v2/payment", payment);
 app.use("/api/v2/withdraw", withdraw);
 
 // it's for ErrorHandling
